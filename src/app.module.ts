e6c979f1b14bfc5087282feef0d5a4308a630940
Booker <nicolaspatino77@gmail.com>
2026-04-20 20:40:32 +0200

    :sparkles: add auth configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { authConfig } from './config/auth.config';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [authConfig],
     }),
     PrismaModule,
   ],
