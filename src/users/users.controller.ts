1d1d1e18d94b20ca129c3f9089cb7560c0ba40ee
Booker <nicolaspatino77@gmail.com>
2026-06-09 21:07:45 +0200

    :sparkles: add GET /users/me/stats endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,22 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import type { PublicUser } from './types/public-user.type';
+import type { UserStats } from './types/user-stats.type';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get('me')
   @UseGuards(JwtAuthGuard)
   me(@CurrentUser() user: PublicUser): PublicUser {
     return user;
+  }
+
+  @Get('me/stats')
+  @UseGuards(JwtAuthGuard)
+  stats(@CurrentUser() user: PublicUser): Promise<UserStats> {
+    return this.usersService.getStats(user.id);
   }
 }
